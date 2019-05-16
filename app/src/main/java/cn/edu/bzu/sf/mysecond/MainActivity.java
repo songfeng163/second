f2e1992036cb178e77a8967d731f1a9479f15f83
admin <songfeng163@126.com>
2019-05-16 18:15:49 +0800

    20190516宋锋做了些更新

--- app/src/main/java/cn/edu/bzu/sf/mysecond/MainActivity.java
+++ app/src/main/java/cn/edu/bzu/sf/mysecond/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+    }
+
+    private void initView(){
+
+    }
+
+    private void initData(){
+
     }
 }
