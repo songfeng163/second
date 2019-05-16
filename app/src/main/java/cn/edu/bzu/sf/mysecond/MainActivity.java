d97dc81a92c88a48ed1497ef727e37c065cfde47
admin <songfeng163@126.com>
2019-05-16 18:04:08 +0800

    20190516宋锋创建项目

---
+++ app/src/main/java/cn/edu/bzu/sf/mysecond/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.bzu.sf.mysecond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
